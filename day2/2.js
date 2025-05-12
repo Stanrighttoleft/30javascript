3a38dcf1aa57a75a3a0ee3b20934969eedc1982a
stan <moon_saga@yahoo.com.tw>
2025-05-12 20:30:59 +0800

    fix the second-hand will run a run when reach 0 second

--- day2/2.js
+++ day2/2.js
@@ -1,22 +1,33 @@
-const secondhand=document.querySelector('.second-hand')
-const minhand=document.querySelector('.min-hand')
-const hourhand=document.querySelector('.hour-hand')
+    const secondhand=document.querySelector('.second-hand')
+    const minhand=document.querySelector('.min-hand')
+    const hourhand=document.querySelector('.hour-hand')
 
-function setDate(){
-    const now= new Date();
-    const second=now.getSeconds();
-    const secondDegree=((second/60)*360)+90;
-    secondhand.style.transform=`rotate(${secondDegree}deg)`;
+    function setDate(){
+        const now= new Date();
+        let second=now.getSeconds();
+        let secondDegree=((second/60)*360)+90;
+      
+        if(second===0){
+                        
+            secondhand.style.transition='none';
+        }else{
+            secondhand.style.transition=`all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)`;
+            
 
-    const min=now.getMinutes();
-    const minDegree=((min/60)*360)+90;
-    minhand.style.transform=`rotate(${minDegree}deg)`;
+        }
+       
+        secondhand.style.transform=`rotate(${secondDegree}deg)`;
+        
 
-    const hour=now.getHours();
-    const hourDegree=((hour/12)*360)+90;
-    hourhand.style.transform=`rotate(${hourDegree}deg)`;
+        let min=now.getMinutes();
+        let minDegree=((min/60)*360)+90;
+        minhand.style.transform=`rotate(${minDegree}deg)`;
 
-}
+        let hour=now.getHours();
+        let hourDegree=((hour/12)*360)+90;
+        hourhand.style.transform=`rotate(${hourDegree}deg)`;
+
+    }
 
 
-setInterval(setDate,1000);
+    setInterval(setDate,1000);
