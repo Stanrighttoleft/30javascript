d7629190c49ab1140d30fccea3cbfb2d57d77026
stan <moon_saga@yahoo.com.tw>
2025-06-15 22:10:21 +0800

    29

--- day29-countdown/29.js
+++ day29-countdown/29.js
@@ -1,16 +1,27 @@
 let coutdown;
+const timerDisplay=document.querySelector('.display__time-left');
+
 function timer(seconds){
     const now=Date.now();
     const then=now+seconds *1000;
+    displaytimeLeft(seconds);
     
 
-    countdown=setInterval(()=>{
+    let countdown=setInterval(()=>{
         const secondsLeft=Math.round((then-Date.now())/1000);
         
         if(secondsLeft<0){
             clearInterval(countdown);
         }else{
-            console.log(secondsLeft);
+            displaytimeLeft(secondsLeft)    
         }
-    },1000)
+    },1000);
+}
+
+function displaytimeLeft(seconds){
+    const mins=Math.floor(seconds/60);
+    const second=seconds%60;
+    const display=`${mins}:${second<10 ? '0': ' '}${second}`
+    timerDisplay.textContent=display;
+    console.log({mins,second});
 }
