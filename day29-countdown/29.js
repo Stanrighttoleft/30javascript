fa39ac77759ceabed430e0b5416b973572fbf481
stan <moon_saga@yahoo.com.tw>
2025-06-16 21:31:44 +0800

    29

--- day29-countdown/29.js
+++ day29-countdown/29.js
@@ -1,10 +1,12 @@
 let coutdown;
 const timerDisplay=document.querySelector('.display__time-left');
+const endTime=document.querySelector('.display__end-time')
 
 function timer(seconds){
     const now=Date.now();
     const then=now+seconds *1000;
     displaytimeLeft(seconds);
+    displayEndTime(then);
     
 
     let countdown=setInterval(()=>{
@@ -24,4 +26,11 @@ function displaytimeLeft(seconds){
     const display=`${mins}:${second<10 ? '0': ' '}${second}`
     timerDisplay.textContent=display;
     console.log({mins,second});
+}
+function displayEndTime(timestamp){
+    const end=new Date(timestamp);
+    const hours=end.getHours();
+    const mins=end.getMinutes();
+    endTime.textContent=`Be back at ${hours}:${mins}`;
+
 }
