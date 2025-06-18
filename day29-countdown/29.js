762f8d64f08b7cab62277a2494c008e6d85c3400
stan <moon_saga@yahoo.com.tw>
2025-06-18 22:19:49 +0800

    29

--- day29-countdown/29.js
+++ day29-countdown/29.js
@@ -1,15 +1,18 @@
-let coutdown;
+let countdown;
 const timerDisplay=document.querySelector('.display__time-left');
 const endTime=document.querySelector('.display__end-time')
+const buttons=document.querySelectorAll('[data-time]')
 
 function timer(seconds){
+    //clear any existing timer when we start the timer
+    clearInterval(countdown);
     const now=Date.now();
     const then=now+seconds *1000;
     displaytimeLeft(seconds);
     displayEndTime(then);
     
 
-    let countdown=setInterval(()=>{
+   countdown=setInterval(()=>{
         const secondsLeft=Math.round((then-Date.now())/1000);
         
         if(secondsLeft<0){
@@ -34,3 +37,20 @@ function displayEndTime(timestamp){
     endTime.textContent=`Be back at ${hours>12 ? hours-12 :hours}:${mins<10 ?"0" :''}${mins}`;
 
 }
+
+function startTimer(){
+    seconds=parseInt(this.dataset.time);
+    timer(seconds);
+    
+}
+
+
+
+buttons.forEach(button=>button.addEventListener('click',startTimer))
+document.customForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const mins=this.minutes.value;
+    console.log(mins);
+    timer(mins*60);
+    this.reset();
+})
