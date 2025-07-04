585b1169b0a0ca4bc9b364e78be126e12c240d8e
stan <moon_saga@yahoo.com.tw>
2025-07-05 00:35:00 +0800

    finish practice

--- word-typing/index2.js
+++ word-typing/index2.js
@@ -1,6 +1,25 @@
-const containerEl=document.querySelector('container');
+const containerEl=document.querySelector('.container');
 
-containerEl.innerHTML=`
-<h1>I am a
+let careerIndex=0;
+let characterIndex=0;
+let career=['Youtuber', 'Instructor', 'Teacher', 'Student']
 
-`
+updatecontent();
+
+function updatecontent(){
+    characterIndex++;
+    containerEl.innerHTML=`
+    <h1>I am ${career[careerIndex].slice(0,1)==="I"?"an":"a"} ${career[careerIndex].slice(0,characterIndex)} <h1>
+
+    `
+    if(characterIndex===career[careerIndex].length){
+        careerIndex++;
+        characterIndex=0;
+    }
+    if(careerIndex===career.length){
+        careerIndex=0;
+        characterIndex=0;
+    }
+
+    setTimeout(updatecontent,400);
+}
